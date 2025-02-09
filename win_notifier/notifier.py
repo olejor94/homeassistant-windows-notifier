2eb2797a2d323729e6cc263ba776de011274feb3
olejor94 <80732002+olejor94@users.noreply.github.com>
2025-02-09 17:10:36 +0100

    Update notifier.py

--- win_notifier/notifier.py
+++ win_notifier/notifier.py
@@ -1,6 +1,6 @@
-from win10toast import ToastNotifier
 import json
 import time
+from win10toast import ToastNotifier
 
 toaster = ToastNotifier()
 
@@ -8,7 +8,10 @@ def send_notification(message):
     toaster.show_toast("Home Assistant", message, duration=5)
 
 while True:
-    with open("notification.json", "r") as file:
-        data = json.load(file)
-        send_notification(data.get("message", "No message"))
+    try:
+        with open("notification.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+            send_notification(data.get("message", "No message"))
+    except Exception as e:
+        send_notification(f"Error: {str(e)}")
     time.sleep(10)

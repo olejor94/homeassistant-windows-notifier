5e2ce8ee44cbed8d442909b0059d6dcd28636213
olejor94 <80732002+olejor94@users.noreply.github.com>
2025-02-09 16:56:40 +0100

    Create notifier.py

---
+++ notifier.py
@@ -0,0 +1,14 @@
+from win10toast import ToastNotifier
+    import json
+    import time
+
+    toaster = ToastNotifier()
+
+    def send_notification(message):
+        toaster.show_toast("Home Assistant", message, duration=5)
+
+    while True:
+        with open("notification.json", "r") as file:
+            data = json.load(file)
+            send_notification(data.get("message", "No message"))
+        time.sleep(10)
